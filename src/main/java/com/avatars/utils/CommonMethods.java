20005cb10c919fae14c8388d76c36ba28600cf49
iamdez99 <diegojuarezbusiness@gmail.com>
2024-05-29 21:06:19 -0400

    ..

--- src/main/java/com/avatars/utils/CommonMethods.java
+++ src/main/java/com/avatars/utils/CommonMethods.java
@@ -5,17 +5,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class CommonMethods {
 
     public static WebDriver driver;
 
-    public static void openChromeBrowser() {
+    /**
+     * Opens a Chrome browser and navigates to the specified URL.
+     *
+     * This method initializes a new instance of the ChromeDriver, maximizes the browser window,
+     * deletes all cookies, sets the implicit wait timeout to 10 seconds, and navigates to the
+     * specified URL.
+     *
+     * @param url the URL to navigate to
+     */
+    public static void openChromeBrowserAndNavigateToSite(String url) {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(url);
     }
 
-    public static void scrollIntoView(WebElement element){
+    /**
+     * Scrolls the webpage to bring the specified element into view.
+     *
+     * This method uses JavaScript executor to scroll the webpage so that the specified element
+     * is brought into view. It sets the "scrollIntoView" property of the element to "true".
+     *
+     * @param element the WebElement to scroll into view
+     */
+    public static void scrollIntoView(WebElement element) {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         jsExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
     }
--- src/test/java/com/avatars/AlertsAndModalDialogs.java
+++ src/test/java/com/avatars/AlertsAndModalDialogs.java
@@ -18,15 +18,10 @@ public class AlertsAndModalDialogs {
         CommonMethods.openChromeBrowser();
 
         String url = "https://chromatechacademy.net/selenium-practice/";
-
         driver.get(url);
 
         WebElement alertButton = driver.findElement(By.xpath("//input[@value='Alert']"));
-//        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-//        jsExecutor.executeScript("arguments[0].scrollIntoView(true);", alertButton);
-
         CommonMethods.scrollIntoView(alertButton);
-
 
         Thread.sleep(2000);
 
