c5020a6a4ab224e0cf4972644baba645adc4bba9
iamdez99 <diegojuarezbusiness@gmail.com>
2024-05-30 21:31:36 -0400

    ..

--- src/test/java/com/avatars/FindElementVsFindElements.java
+++ src/test/java/com/avatars/FindElementVsFindElements.java
@@ -1,32 +1,20 @@
 package com.avatars;
 
+import com.avatars.utils.CommonMethods;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.List;
+import static com.avatars.utils.CommonMethods.driver;
 
 public class FindElementVsFindElements {
 
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
+    public static void main(String[] args) {
 
-        String url = "https://chromatechacademy.net/selenium-practice/";
-
-        driver.get(url);
-
+        CommonMethods.openChromeBrowserAndNavigateToSite("https://chromatechacademy.net/selenium-practice/");
         boolean flag = driver.findElement(By.xpath("//button[@class='btn btn-sm dropdown-toggle btn-outline-light']")).isDisplayed();
         System.out.println("IS DROPDOWN DISPLAYED: " + flag);
-
         List<WebElement> list = driver.findElements(By.xpath("//*[@value='asdfasdfasdfasdf']"));
-
         System.out.println("Number of elements found: " + list.size());
-
-
-        Thread.sleep(3000);
         driver.quit();
     }
 }
