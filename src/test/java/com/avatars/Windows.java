02f14db0f54e73cd7bbed0969426ac8bc63d38d9
iamdez99 <diegojuarezbusiness@gmail.com>
2024-05-31 21:36:11 -0400

    ..

--- src/test/java/com/avatars/Windows.java
+++ src/test/java/com/avatars/Windows.java
@@ -1,42 +1,18 @@
 package com.avatars;
 
-import java.util.Set;
-
+import com.avatars.utils.CommonMethods;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import static com.avatars.utils.CommonMethods.driver;
 
 public class Windows {
     
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-
-        String url = "https://chromatechacademy.net/selenium-practice/";
-
-        driver.get(url);
-
+    public static void main(String[] args) {
+        CommonMethods.openChromeBrowserAndNavigateToSite("https://chromatechacademy.net/selenium-practice/");
         WebElement openWindowButton = driver.findElement(By.xpath("//button[contains(text(),'Open Window')]"));
-        Thread.sleep(2000);
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
-        jsExecutor.executeScript("arguments[0].scrollIntoView(true);", openWindowButton);
-        Thread.sleep(2000);
-
+        CommonMethods.scrollIntoView(openWindowButton);
         openWindowButton.click();
-
-        Set<String> windows = driver.getWindowHandles();
-
-        for(String window : windows){
-            driver.switchTo().window(window);
-        }
-
-        Thread.sleep(2000);
-        //driver.close();
-
+        CommonMethods.switchToNextWindow();
         driver.quit();
     }
-
 }
