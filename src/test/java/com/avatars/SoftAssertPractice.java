f1556bbf1ec221533156546a462451b20f2a7d3c
iamdez99 <diegojuarezbusiness@gmail.com>
2024-05-31 20:57:39 -0400

    ..

--- src/test/java/com/avatars/SoftAssertPractice.java
+++ src/test/java/com/avatars/SoftAssertPractice.java
@@ -1,36 +1,21 @@
 package com.avatars;
 
+import com.avatars.utils.CommonMethods;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
-
-import java.time.Duration;
+import static com.avatars.utils.CommonMethods.driver;
 
 public class SoftAssertPractice {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        String url = "https://chromatechacademy.net/selenium-practice/";
-        driver.get(url);
-
+        CommonMethods.openChromeBrowserAndNavigateToSite("https://chromatechacademy.net/selenium-practice/");
         String actualPageTitle = driver.getTitle();
         String expectedPageTitle = "Chroma Tech Academylkjh;lkjh";
-
         SoftAssert softAssert = new SoftAssert();
-
         softAssert.assertEquals(actualPageTitle, expectedPageTitle);
-
         String actualHeaderText = driver.findElement(By.xpath("//h1[normalize-space()='Chroma Tech Academy Selenium Practice']")).getText();
-
         softAssert.assertEquals(actualHeaderText, "Chroma Tech Academy Selenium Practice!!!!!");
-
         softAssert.assertAll();
-
         driver.quit();
     }
 }
