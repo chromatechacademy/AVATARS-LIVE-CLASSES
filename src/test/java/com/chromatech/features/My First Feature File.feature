5072a653e509fbb3e7ef39224116727f17211bc6
iamdez99 <diegojuarezbusiness@gmail.com>
2024-06-11 22:07:11 -0400

    ..

--- src/test/java/com/chromatech/features/My First Feature File.feature
+++ src/test/java/com/chromatech/features/My First Feature File.feature
@@ -2,7 +2,8 @@ Feature: My first feature file
 
   @RunTest
   Scenario: My first test case
-    Given a user is on the Chroma Tech Academy practice site
-    When user enters username in username text box
+    Given a user is on the Chroma Tech Academy practice site ""
+    When user enters username "general@teacher.com" in username text box
     And enters password in password text box
     And clicks on Sign In button
+    Then user is directed to the CTSMS dashboard page
--- src/test/java/com/chromatech/steps/My_First_Feature_File_Steps.java
+++ src/test/java/com/chromatech/steps/My_First_Feature_File_Steps.java
@@ -3,21 +3,17 @@ package com.chromatech.steps;
 import com.avatars.utils.CommonMethods;
 import com.avatars.utils.WebDriverUtils;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class My_First_Feature_File_Steps {
 
     @Given("a user is on the Chroma Tech Academy practice site")
     public void a_user_is_on_the_chroma_tech_academy_practice_site() {
         WebDriverUtils.driver.get("https://chroma.mexil.it/site/login");
-    }
-
-    @When("user enters username in username text box")
-    public void user_enters_username_in_username_text_box() {
-        WebElement usernameTextBox = WebDriverUtils.driver.findElement(By.xpath("//input[@id='form-username']"));
-        usernameTextBox.sendKeys("general@teacher.com");
     }
 
     @When("enters password in password text box")
@@ -32,4 +28,18 @@ public class My_First_Feature_File_Steps {
        signInButton.click();
        CommonMethods.sleep(3000);
     }
+
+    @Then("user is directed to the CTSMS dashboard page")
+    public void user_is_directed_to_the_ctsms_dashboard_page() {
+        String actualURL = WebDriverUtils.driver.getCurrentUrl();
+        String expectedURL = "https://mexil.it/chroma/admin/admin/dashboard";
+        Assert.assertEquals(actualURL, expectedURL);
+    }
+
+    @When("user enters username {string} in username text box")
+    public void user_enters_username_in_username_text_box(String username) {
+        WebElement usernameTextBox = WebDriverUtils.driver.findElement(By.xpath("//input[@id='form-username']"));
+        usernameTextBox.sendKeys(username);
+    }
+
 }
